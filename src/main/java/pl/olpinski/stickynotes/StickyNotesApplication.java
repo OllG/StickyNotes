0e7fa04f18abd00662046edf8fa0d2b0b1f206bd
olg <olgierd@olpinski.pl>
2018-12-13 11:26:28 +0100

    first commit

---
+++ src/main/java/pl/olpinski/stickynotes/StickyNotesApplication.java
@@ -0,0 +1,14 @@
+package pl.olpinski.stickynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickyNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StickyNotesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/olpinski/stickynotes/StickyNotesApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.olpinski.stickynotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StickyNotesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
