ebf85c2e0fc20d6377543c941c95e88b2e927409
olg <olgierd@olpinski.pl>
2018-12-22 17:28:38 +0100

    Little tweaks on notes template and added more records in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,8 @@ insert into USER (id, login, password, creation_time, status, mail, first_name, 
 insert into NOTE (id, title, content, user_id) VALUES (1, 'Pierwsza notatka', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
 insert into NOTE (id, title, content, user_id) VALUES (2, 'Druga notatka', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
 insert into NOTE (id, title, content, user_id) VALUES (3, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
+insert into NOTE (id, title, content, user_id) VALUES (4, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
+insert into NOTE (id, title, content, user_id) VALUES (5, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
+insert into NOTE (id, title, content, user_id) VALUES (6, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
+insert into NOTE (id, title, content, user_id) VALUES (7, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
+insert into NOTE (id, title, content, user_id) VALUES (8, 'Trzecia notatka', 'ĄĘLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 1);
