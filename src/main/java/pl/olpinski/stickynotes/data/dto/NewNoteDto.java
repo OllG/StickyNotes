93bd27bd8c68260509850c9b7387f9bb9589e6ba
Olg <o.olpinski@gmail.com>
2019-01-08 18:12:42 +0100

    Refactoring dto packet

---
+++ src/main/java/pl/olpinski/stickynotes/data/dto/NewNoteDto.java
@@ -0,0 +1,30 @@
+package pl.olpinski.stickynotes.data.dto;
+
+public class NewNoteDto {
+
+    private Long userId;
+    private String title;
+    private String content;
+
+    public NewNoteDto(Long userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
